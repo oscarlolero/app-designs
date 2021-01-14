ef6f5e9c6693c711ba217c8a2934861e2129dc38
Oscar Montes <oscmcojc1@live.com.mx>
2021-01-14 16:45:04 -0600

    Design with scroll implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:designs/src/pages/basic_page.dart';
 import 'package:flutter/material.dart';
+
+import 'package:designs/src/pages/basic_page.dart';
+import 'package:designs/src/pages/scroll_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,9 +11,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Diseños',
-      initialRoute: 'basic',
+      initialRoute: 'scroll',
       routes: {
-        'basic': (BuildContext context) => BasicPage()
+        'basic': (BuildContext context) => BasicPage(),
+        'scroll': (BuildContext context) => ScrollPage(),
       },
     );
   }
---
+++ lib/src/pages/scroll_page.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class ScrollPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        scrollDirection: Axis.vertical,
+        children: [
+          _page1(),
+          _page2(),
+        ],
+      ),
+    );
+  }
+
+  Widget _page1() {
+    return Stack(
+      children: [_backgroundColor(), _loadBackground(), _texts()],
+    );
+  }
+
+  Widget _page2() {
+    return Stack(
+      children: [
+        _backgroundColor(),
+        Center(
+          child: RaisedButton(
+            padding: EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+            shape: StadiumBorder(),
+            color: Colors.blue,
+            onPressed: () {},
+            child: Text(
+              'Bienvenido',
+              style: TextStyle(color: Colors.white, fontSize: 19),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _texts() {
+    final textStyle = TextStyle(color: Colors.white, fontSize: 50.0);
+
+    return SafeArea(
+      child: Column(
+        children: [
+          SizedBox(height: 20.0),
+          Text('11°', style: textStyle),
+          Text('Miércoles', style: textStyle),
+          Expanded(child: Container()),
+          Icon(Icons.keyboard_arrow_down, size: 70, color: Colors.white)
+        ],
+      ),
+    );
+  }
+
+  Widget _backgroundColor() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Colors.lightBlueAccent,
+    );
+  }
+
+  Widget _loadBackground() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: Image(
+        image: AssetImage('assets/scroll-1.png'),
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
